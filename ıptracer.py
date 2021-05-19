bb6ac088cc6e0a84ef0e0f4199c32a2e6cbca976
hawk.0fcx <67156465+hawk-unity@users.noreply.github.com>
2021-05-19 03:33:10 +0300

    Add files via upload

---
+++ ıptracer.py
@@ -0,0 +1,21 @@
+import os
+import sys
+import colorama
+import time
+from colorama import Fore, Back, Style, init
+print(Fore.BLUE +  "[+] ----------------HAWK-UNİTY----------------------[+]")
+os.system("screenfetch")
+print(Fore.BLUE +  "[+] ----------------HAWK-UNİTY----------------------[+]")
+time.sleep(5)
+os.system("clear")
+print(Fore.BLUE +  "[+] ----------------HAWK-UNİTY----------------------[+]")
+print(Fore.GREEN + "[-] Author : HAWK UNİTY ")
+print(Fore.GREEN + "[-] GİT HUB : https://github.com/hawk-unity ")
+print(Fore.GREEN + "[-] İNSTAGRAM : https://instagram.com/4zusagawa ")
+print(Fore.GREEN + "[-] YOU TUBE : Bir Coder ")
+print(Fore.BLUE +  "[+] ----------------HAWK-UNİTY----------------------[+]")
+print(Fore.BLUE +  "[+] ----------------HAWK-UNİTY----------------------[+]")
+target= input(Fore.GREEN + "[+]Target -> : ")
+os.system("curl http://ip-api.com/" +target)
+print(Fore.GREEN + "[+]İp :  " + target )
+print("[+]succes ip trace ")
